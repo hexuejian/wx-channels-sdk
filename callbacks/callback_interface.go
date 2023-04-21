afe6eaa105b94b1c56555366850a27fcc5c1719a
xuejian.he <492729589@qq.com>
2023-04-21 19:30:09 +0800

    优惠券回调

--- callbacks/callback_interface.go
+++ callbacks/callback_interface.go
@@ -12,3 +12,24 @@ type CallbackExtraInfoInterface interface {
 func supportCallback(item CallbackExtraInfoInterface) {
 	callbackParseExtraInfoMap[item.GetTypeKey()] = item
 }
+
+func SupportSdkCallback() {
+	item1 := ChannelsEcCouponInvalid{}
+	callbackParseExtraInfoMap[item1.GetTypeKey()] = item1
+	item2 := ChannelsEcCouponDelete{}
+	callbackParseExtraInfoMap[item2.GetTypeKey()] = item2
+	item3 := ChannelsEcUserCouponUse{}
+	callbackParseExtraInfoMap[item3.GetTypeKey()] = item3
+	item4 := ChannelsEcCouponExpire{}
+	callbackParseExtraInfoMap[item4.GetTypeKey()] = item4
+	item5 := ChannelsEcUserCouponUnuse{}
+	callbackParseExtraInfoMap[item5.GetTypeKey()] = item5
+	item6 := ChannelsEcCouponReceive{}
+	callbackParseExtraInfoMap[item6.GetTypeKey()] = item6
+	item7 := ChannelsEcCouponCreate{}
+	callbackParseExtraInfoMap[item7.GetTypeKey()] = item7
+	item8 := ChannelsEcCouponInfoChange{}
+	callbackParseExtraInfoMap[item8.GetTypeKey()] = item8
+	item9 := ChannelsEcUserCouponExpire{}
+	callbackParseExtraInfoMap[item9.GetTypeKey()] = item9
+}
